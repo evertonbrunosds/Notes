907b0bb3b68b25dd584246f91ad3f4212b26f558
evertonbrunosds <evertonbrunosds@gmail.com>
2024-08-04 06:43:28 -0300

    feature: add annotation validation

---
+++ backend/src/main/java/com/github/evertonbrunosds/notes/validation/annotation/Email.java
@@ -0,0 +1,28 @@
+package com.github.evertonbrunosds.notes.validation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+
+@NotBlank
+@Length(max = 64, message = "The length must be less than or equal to 64")
+@jakarta.validation.constraints.Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+@Constraint(validatedBy = {})
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Email {
+
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
